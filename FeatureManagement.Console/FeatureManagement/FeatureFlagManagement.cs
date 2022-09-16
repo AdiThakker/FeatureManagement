71c4df14a215ee15343aca31ec210746d294e163
Adi Thakker <Aditya.Thakker@gmail.com>
2022-09-16 17:06:42 -0400

    more WIP changes

--- FeatureManagement.Console/FeatureManagement/FeatureFlagManagement.cs
+++ FeatureManagement.Console/FeatureManagement/FeatureFlagManagement.cs
@@ -1,9 +1,19 @@
 ﻿using FeatureManagement.Console.FeatureManagement.Interfaces;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
 
 namespace FeatureManagement.Console.FeatureManagement
 {
     public class FeatureFlagManagement : IFeatureFlagManagement
     {
+        private readonly ILogger<FeatureFlagManagement> logger;
+
+        private readonly IFeatureManager featureManager;
+
+        public FeatureFlagManagement(ILogger<FeatureFlagManagement> logger, IFeatureManager featureManager) 
+            => (this.logger, this.featureManager) = (logger, featureManager);
+        
+
         public Task<TResult> ExecuteIfFeatureEnabledAsync<TResult>(Func<TResult> executeFunc)
         {
             throw new NotImplementedException();
@@ -17,6 +27,16 @@ namespace FeatureManagement.Console.FeatureManagement
         public Task<bool> IsFeatureEnabledAsync<TContext>(string name, TContext context)
         {
             throw new NotImplementedException();
+        }
+
+        private async Task<bool> IsFeatureEnabledAsync<TContext>(string feature, FeatureFlagDefaultState defaultState, TContext context)
+        {
+            return context switch
+            {
+                null => await featureManager.IsEnabledAsync(feature),
+                _ => await featureManager.IsEnabledAsync(feature, context)
+            };
+                
         }
     }
 }
