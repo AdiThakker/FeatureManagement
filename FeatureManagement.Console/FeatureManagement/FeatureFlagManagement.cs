17b9adf19bfdd08fd12fe8d52e63485ed5d3c11d
Adi Thakker <Aditya.Thakker@gmail.com>
2022-09-19 17:18:30 -0400

    more changes for Feature Flags

--- FeatureManagement.Console/FeatureManagement/FeatureFlagManagement.cs
+++ FeatureManagement.Console/FeatureManagement/FeatureFlagManagement.cs
@@ -1,6 +1,7 @@
 ﻿using FeatureManagement.Console.FeatureManagement.Interfaces;
 using Microsoft.Extensions.Logging;
 using Microsoft.FeatureManagement;
+using System.Reflection;
 
 namespace FeatureManagement.Console.FeatureManagement
 {
@@ -10,33 +11,58 @@ namespace FeatureManagement.Console.FeatureManagement
 
         private readonly IFeatureManager featureManager;
 
-        public FeatureFlagManagement(ILogger<FeatureFlagManagement> logger, IFeatureManager featureManager) 
+        public FeatureFlagManagement(ILogger<FeatureFlagManagement> logger, IFeatureManager featureManager)
             => (this.logger, this.featureManager) = (logger, featureManager);
-        
 
-        public Task<TResult> ExecuteIfFeatureEnabledAsync<TResult>(Func<TResult> executeFunc)
+
+        public async Task<TResult> ExecuteIfFeatureEnabledAsync<TResult>(Func<TResult> executeFunc)
+        {
+            FeatureDefinitionAttribute? flag = GetAttribute<Type>(executeFunc.GetType());
+            if (await IsFeatureEnabledAsyncHelper<object>(flag!.FeatureName, flag.DefaultState, null))
+                return executeFunc();
+            else
+                return default;
+        }
+
+        public async Task<TResult> ExecuteIfFeatureEnabledAsync<TResult, TContext>(Func<TResult> executeFunc, TContext context)
+        {
+            FeatureDefinitionAttribute? flag = GetAttribute<Type>(executeFunc.GetType());
+            if (await IsFeatureEnabledAsyncHelper<object>(flag!.FeatureName, flag.DefaultState, context))
+                return executeFunc();
+            else
+                return default;
+        }
+
+        public async Task<bool> IsFeatureEnabledAsync(string name) => await IsFeatureEnabledAsyncHelper<object>(name, FeatureFlagDefaultState.Unkown, null);
+
+        public async Task<bool> IsFeatureEnabledAsync<TContext>(string name, TContext context) => await IsFeatureEnabledAsyncHelper<object>(name, FeatureFlagDefaultState.Unkown, context);
+
+        public async Task<bool> IsFeatureEnabledAsync<TType>(TType instance) where TType : class
+        {
+            FeatureDefinitionAttribute? flag = GetAttribute<Type>(instance.GetType());
+            return await IsFeatureEnabledAsyncHelper<object>(flag!.FeatureName, flag.DefaultState, null);
+        }
+
+        public async Task<bool> IsFeatureEnabledAsync<TType, TContext>(TType instance, TContext context) where TType : class
+        {
+            FeatureDefinitionAttribute? flag = GetAttribute<Type>(instance.GetType());
+            return await IsFeatureEnabledAsyncHelper<object>(flag!.FeatureName, flag.DefaultState, context);
+        }
+
+        Task<TResult> IFeatureFlagManagement.ExecuteIfFeatureEnabledAsync<TResult>(Func<TResult> executeFunc)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> IsFeatureEnabledAsync(string name)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> IsFeatureEnabledAsync<TContext>(string name, TContext context)
-        {
-            throw new NotImplementedException();
-        }
-
-        private async Task<bool> IsFeatureEnabledAsync<TContext>(string feature, FeatureFlagDefaultState defaultState, TContext context)
+        private async Task<bool> IsFeatureEnabledAsyncHelper<TContext>(string feature, FeatureFlagDefaultState defaultState, TContext? context)
         {
             return context switch
             {
                 null => await featureManager.IsEnabledAsync(feature),
                 _ => await featureManager.IsEnabledAsync(feature, context)
             };
-                
         }
+
+        private FeatureDefinitionAttribute? GetAttribute<TResult>(Type executingType) => executingType.GetCustomAttributes(true).OfType<FeatureDefinitionAttribute>().FirstOrDefault();
     }
 }
--- FeatureManagement.Console/FeatureManagement/Interfaces/IFeatureFlagManagement.cs
+++ FeatureManagement.Console/FeatureManagement/Interfaces/IFeatureFlagManagement.cs
@@ -4,8 +4,14 @@
     {
         Task<bool> IsFeatureEnabledAsync(string name);
 
+        Task<bool> IsFeatureEnabledAsync<TType>(TType instance) where TType: class;
+
         Task<bool> IsFeatureEnabledAsync<TContext>(string name, TContext context);
 
+        Task<bool> IsFeatureEnabledAsync<TType, TContext>(TType instance, TContext context) where TType : class;
+
         Task<TResult> ExecuteIfFeatureEnabledAsync<TResult>(Func<TResult> executeFunc);
+
+        Task<TResult> ExecuteIfFeatureEnabledAsync<TResult, TContext>(Func<TResult> executeFunc, TContext context);
     }
 }
